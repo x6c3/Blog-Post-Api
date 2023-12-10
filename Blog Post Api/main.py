59912805a351acd853e69682da468d802a6c083c
x86 <90693790+x86642@users.noreply.github.com>
2023-12-10 12:01:43 +0530

    Add files via upload

---
+++ Blog Post Api/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, HTTPException
+import json
+from pydantic import BaseModel
+import mysql.connector
+from datetime import datetime, date
+
+app = FastAPI()
+conn = mysql.connector.connect(host='localhost', user='root', password='', db='my-db')
+cursor = conn.cursor()
+
+class Posts(BaseModel):
+	name: str
+	timestamp: date
+	post: str
+
+@app.get('/x86/api/home')
+def home():
+	return {'Message': 'Hello world'}
+
+@app.get('/x86/api/viewall')
+def get_all_posts():
+	sql = 'SELECT * FROM POSTS'
+	cursor.execute(sql)
+	posts = cursor.fetchall()
+
+	if posts is not None:
+		return json.dumps({'Blogs': posts})
+	else:
+		return json.dumps({'Error': 'No Posts Found'})
+
+@app.post('/x86/api/createpost')
+def create_post(posts: Posts):
+	sql = 'INSERT INTO posts VALUES (%s, %s, %s)'
+	values = (posts.name, posts.timestamp, posts.post)
+
+	try:
+		cursor.execute(sql, values)
+		conn.commit()
+		return json.dumps({'Message': 'Succesfully created your post'})
+	except Exception as e:
+		return json.dumps({f'Error': 'Something went wrong {str{e}}'})
+
+
+
