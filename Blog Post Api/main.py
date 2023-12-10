a6ad9d2c913e1db3d43633972ab8444f362cf3cd
jxytherealone <thushar17223@gmail.com>
2023-12-10 06:43:53 +0530

    HI

--- Blog Post Api/main.py
+++ Blog Post Api/main.py
@@ -2,43 +2,40 @@ from fastapi import FastAPI, HTTPException
 import json
 from pydantic import BaseModel
 import mysql.connector
-from datetime import datetime, date
+from datetime import date, datetime
 
 app = FastAPI()
 conn = mysql.connector.connect(host='localhost', user='root', password='', db='my-db')
 cursor = conn.cursor()
 
 class Posts(BaseModel):
-	name: str
-	timestamp: date
-	post: str
+    name: str
+    timestamp: date
+    post: str
 
 @app.get('/x86/api/home')
 def home():
-	return {'Message': 'Hello world'}
+    return {'Message': 'Hello world'}
 
 @app.get('/x86/api/viewall')
 def get_all_posts():
-	sql = 'SELECT * FROM POSTS'
-	cursor.execute(sql)
-	posts = cursor.fetchall()
+    sql = 'SELECT * FROM POSTS'
+    cursor.execute(sql)
+    posts = cursor.fetchall()
 
-	if posts is not None:
-		return json.dumps({'Blogs': posts})
-	else:
-		return json.dumps({'Error': 'No Posts Found'})
+    if posts is not None:
+        return json.dumps({'Blogs': posts})
+    else:
+        return json.dumps({'Error': 'No Posts Found'})
 
 @app.post('/x86/api/createpost')
 def create_post(posts: Posts):
-	sql = 'INSERT INTO posts VALUES (%s, %s, %s)'
-	values = (posts.name, posts.timestamp, posts.post)
+    sql = 'INSERT INTO posts VALUES (%s, %s, %s)'
+    values = (posts.name, posts.timestamp.isoformat(), posts.post)
 
-	try:
-		cursor.execute(sql, values)
-		conn.commit()
-		return json.dumps({'Message': 'Succesfully created your post'})
-	except Exception as e:
-		return json.dumps({f'Error': 'Something went wrong {str{e}}'})
-
-
-
+    try:
+        cursor.execute(sql, values)
+        conn.commit()
+        return json.dumps({'Message': 'Successfully created your post'})
+    except Exception as e:
+        return json.dumps({'Error': f'Something went wrong {str(e)}'})
